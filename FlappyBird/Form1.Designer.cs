a93bae9b7f22781da4f02160c7868945871761cd
HeliturtleOP <47545028+HeliturtleOP@users.noreply.github.com>
2021-05-10 11:56:37 -0500

    Add files via upload

--- FlappyBird/Form1.Designer.cs
+++ FlappyBird/Form1.Designer.cs
@@ -64,7 +64,7 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label1.Location = new System.Drawing.Point(255, 177);
+            this.label1.Location = new System.Drawing.Point(255, 174);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(245, 47);
             this.label1.TabIndex = 1;
@@ -77,16 +77,16 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(91, 26);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(47, 13);
             this.label2.TabIndex = 6;
-            this.label2.Text = "label2";
+            this.label2.Text = "Score: 0";
             // 
             // label3
             // 
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label3.Location = new System.Drawing.Point(255, 190);
+            this.label3.Location = new System.Drawing.Point(264, 187);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(245, 47);
             this.label3.TabIndex = 11;
@@ -98,7 +98,7 @@
             // 
             this.pictureBox8.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox8.Image = global::FlappyBird.Properties.Resources.PipeUp_png;
-            this.pictureBox8.Location = new System.Drawing.Point(4, 319);
+            this.pictureBox8.Location = new System.Drawing.Point(5, 319);
             this.pictureBox8.Name = "pictureBox8";
             this.pictureBox8.Size = new System.Drawing.Size(58, 295);
             this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -109,7 +109,7 @@
             // 
             this.pictureBox9.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox9.Image = global::FlappyBird.Properties.Resources.PipeDown_png;
-            this.pictureBox9.Location = new System.Drawing.Point(4, -227);
+            this.pictureBox9.Location = new System.Drawing.Point(5, -227);
             this.pictureBox9.Name = "pictureBox9";
             this.pictureBox9.Size = new System.Drawing.Size(58, 295);
             this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -120,7 +120,7 @@
             // 
             this.pictureBox6.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox6.Image = global::FlappyBird.Properties.Resources.PipeUp_png;
-            this.pictureBox6.Location = new System.Drawing.Point(221, 319);
+            this.pictureBox6.Location = new System.Drawing.Point(220, 319);
             this.pictureBox6.Name = "pictureBox6";
             this.pictureBox6.Size = new System.Drawing.Size(58, 295);
             this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -131,7 +131,7 @@
             // 
             this.pictureBox7.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox7.Image = global::FlappyBird.Properties.Resources.PipeDown_png;
-            this.pictureBox7.Location = new System.Drawing.Point(221, -227);
+            this.pictureBox7.Location = new System.Drawing.Point(220, -227);
             this.pictureBox7.Name = "pictureBox7";
             this.pictureBox7.Size = new System.Drawing.Size(58, 295);
             this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -164,7 +164,7 @@
             // 
             this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox3.Image = global::FlappyBird.Properties.Resources.PipeDown_png;
-            this.pictureBox3.Location = new System.Drawing.Point(424, -227);
+            this.pictureBox3.Location = new System.Drawing.Point(425, -227);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(58, 295);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -175,7 +175,7 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(68, 167);
+            this.pictureBox1.Location = new System.Drawing.Point(100, 174);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(50, 50);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -186,7 +186,7 @@
             // 
             this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox2.Image = global::FlappyBird.Properties.Resources.PipeUp_png;
-            this.pictureBox2.Location = new System.Drawing.Point(424, 319);
+            this.pictureBox2.Location = new System.Drawing.Point(425, 319);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(58, 295);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -197,21 +197,24 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBox8);
             this.Controls.Add(this.pictureBox9);
             this.Controls.Add(this.pictureBox6);
             this.Controls.Add(this.pictureBox7);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.pictureBox5);
             this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FlappyBird";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
--- FlappyBird/Form1.cs
+++ FlappyBird/Form1.cs
@@ -7,108 +7,146 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Windows.Input;
+using System.Threading;
 using System.Media;
+using System.Resources;
+using System.Reflection;
 
 namespace FlappyBird
 {
     public partial class Form1 : Form
     {
+        int score = 0;
+        int highscore;
+
+        //pipe speed
         int speed = 5;
 
+        //variables for bird
         int fallSpeed = 0;
         int gravity = 1;
         int jumpVelocity = -10;
 
-        List<PictureBox> pictureBoxList = new List<PictureBox>();
+        //list of pipes
+        List<PictureBox> pipeList = new List<PictureBox>();
+
+        ResourceManager rm = new ResourceManager("items", Assembly.GetExecutingAssembly());
 
         public Form1()
         {
             InitializeComponent();
         }
-
-        private void timer1_Tick(object sender, EventArgs e)
+        
+        //runs once at application start
+        private void Form1_Load(object sender, EventArgs e)
         {
-
-            for (int i = 0; i < pictureBoxList.Count; i++)
+            //populates the list of pipes
+            for (int i = 2; i < 10; i++)
             {
-                
-                if (pictureBoxList[i].Location.X <= -pictureBoxList[i].Size.Width)
-                {
-                    if (i % 2 == 0)
-                    {
-                        Random r = new Random();
-
-                        pictureBoxList[i].Top = r.Next(157, 364);
-                        pictureBoxList[i + 1].Top = pictureBoxList[i].Top - 450;
-                    }
-
-                    pictureBoxList[i].Left = this.Size.Width;                    
-                }
-                pictureBoxList[i].Left -= speed;
-
+                pipeList.Add((PictureBox)Controls.Find("pictureBox" + i, true)[0]);
             }
 
+        }
+
+        //update Loop (Set timer to an interval of 17)
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < pipeList.Count; i++)
+            {
+                //gives score
+                ScoreUp(i);
+                //randomises pipe placement
+                PoolGeneration(i);
+                //moves pipes to the left
+                pipeList[i].Left -= speed;
+            }
+            //method for collision
             Collision();
+            //method for gravity
             Gravity();
+        }
+
+        private void PoolGeneration(int i)
+        {
+            //looks in the pipe list
+            if (pipeList[i].Location.X <= -pipeList[i].Size.Width)
+            {
+                //checks if pipe is on bottom
+                if (i % 2 == 0)
+                {
+                    Random r = new Random();
+
+                    //generats randim number between 157(inclusive) and 364(exclusive) and sets the bottm pipe to that location on the y axis
+                    pipeList[i].Top = r.Next(157, 364);
+                    //sets the location on the y axis off the top pipes to 450 pixels above
+                    pipeList[i + 1].Top = pipeList[i].Top - 450;
+                }
+                //puts pipes off screen to the right on a multiple of 5
+                pipeList[i].Left = (this.Size.Width) / 5 * 5;
+            }
+        }
+
+        private void ScoreUp(int i)
+        {
+            //checks is bird is going through pipe
+            if (pipeList[i].Left == pictureBox1.Left)
+            {
+                //adds score and sets label
+                score++;
+                label2.Text = "Points: " + (score / 2).ToString();
+            }
         }
 
         private void Gravity()
         {
+            //moves the bird down according to fall speed varable;
             pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + fallSpeed);
-            fallSpeed += gravity;          
-            
+            //increases fall speed by gravity to make velocity
+            fallSpeed += gravity;                      
         }
 
         private void Collision()
         {
+            //collision rectangle for bird
             var x1 = pictureBox1.Left;
             var y1 = pictureBox1.Top;
             Rectangle r1 = new Rectangle(x1, y1, pictureBox1.Width, pictureBox1.Height);
 
-            for (int i = 0; i < pictureBoxList.Count; i++)
+            //looks through pipe list
+            for (int i = 0; i < pipeList.Count; i++)
             {
+                //collision rectangle for all pipes;
+                Rectangle r = new Rectangle(pipeList[i].Left, pipeList[i].Top, pictureBox2.Width, pictureBox2.Height);
 
-
-                Rectangle r = new Rectangle(pictureBoxList[i].Left, pictureBoxList[i].Top, pictureBox2.Width, pictureBox2.Height);
-
-                if (r1.IntersectsWith(r))
+                //checks if collision occurs with pipe or ground
+                if (r1.IntersectsWith(r) || pictureBox1.Top >= 400)
                 {
-                    //label1.Visible = true;
-                    //timer1.Stop();
+                    label1.Visible = true;
+                    timer1.Stop();
+                    Int32.TryParse(rm.GetString("HighScore"), out highscore);
+                    if (highscore <= score){
+                        highscore = score;
+                    }
+                    {
+
+                    }
                 }
             }
-
-
-        }
-
-        private void playSimpleSound()
-        {
-            SoundPlayer simpleSound = new SoundPlayer(@".\Bounce.wav");
-            simpleSound.Play();
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
+            //checks if key pressed is space
             if (e.KeyCode == Keys.Space)
             {
+                //sets fall speed to a large positive velocity to make bird "jump" up
                 fallSpeed = jumpVelocity;
-                //playSimpleSound();
             } 
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-            for (int i = 2; i < 10; i++)
-            {
-                pictureBoxList.Add((PictureBox)Controls.Find("pictureBox" + i, true)[0]);               
-            }
-
         }
 
         private void label3_Click(object sender, EventArgs e)
         {
+            //starts the game
             timer1.Enabled = true;
             label3.Visible = false;
         }
